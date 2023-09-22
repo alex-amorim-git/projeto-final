f5ed3adc818dbfb2b3d9df17413b82c14fb15d9a
Alex Amorim <alex.amorim.mail@gmail.com>
2023-09-22 13:39:18 -0300

    Templates

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.DeliveryRoutes import delivery_routes
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates')
 app.register_blueprint(delivery_routes, url_prefix = '/entregas')
+
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
